ef90525ff4d98e7f6168326a88d7661d055c4459
BK <3031011@naver.com>
2023-12-13 10:20:02 +0900

    기본타입

---
+++ 01.ts
@@ -0,0 +1,12 @@
+function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+const bool: boolean = true;
+const num: number = 10;
+const str: string = 'hello';
+const numArr: number[] = [1, 2, 3];
+const strArr: string[] = ['a', 'b', 'c'];
+const obj: object = {
+  name: 'hi',
+};
